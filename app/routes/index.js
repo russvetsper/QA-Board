5bd33127c1d7121452695bd9f4f8c7298585711b
russvetsper <russvetsper@gmail.com>
2016-10-28 12:02:43 -0700

    import date to firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
-  var boards = [{
-    id: 1,
-    author: "mike",
-    question: "whats up",
-    answer: "chilling"
-  }, {
-    id: 2,
-    author: "joe",
-    question: "where you at",
-    answer: "here"
-  }, {
-    id: 3,
-    author: "yi",
-    question: "hungry?",
-    answer: "always"
-  }];
-
-  export default Ember.Route.extend({
-    model() {
-      return boards;
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('board');
   },
 });
