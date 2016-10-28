4486ca26012e4f1250830e86bb740c72ce694ce2
russvetsper <russvetsper@gmail.com>
2016-10-28 11:20:03 -0700

    hard code index js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+  var boards = [{
+    id: 1,
+    author: "mike",
+    question: "whats up",
+    answer: "chilling"
+  }, {
+    id: 2,
+    author: "joe",
+    question: "where you at",
+    answer: "here"
+  }, {
+    id: 3,
+    author: "yi",
+    question: "hungry?",
+    answer: "always"
+  }];
+
+  export default Ember.Route.extend({
+    model() {
+      return boards;
+  },
 });
