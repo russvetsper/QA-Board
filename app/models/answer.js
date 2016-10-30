5cac414b306548d9154ffdac1a5e1b9871a7d298
russvetsper <russvetsper@gmail.com>
2016-10-29 18:13:32 -0700

    add refrence to board and answer

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  review: DS.attr()
+  review: DS.attr(),
+   board: DS.belongsTo('board', { async: true }),
 
 });
--- app/models/board.js
+++ app/models/board.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   author: DS.attr(),
   question: DS.attr(),
   note: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 
 });
