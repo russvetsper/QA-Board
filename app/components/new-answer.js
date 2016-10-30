3850093e000d27a30ec8e062b23141fcef5fe014
russvetsper <russvetsper@gmail.com>
2016-10-29 17:58:03 -0700

    add save answer backend js

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+     var params = {
+       author: this.get('author'),
+       rating: this.get('answer')
+     };
+     this.set('addNewAnswer', false);
+     this.sendAction('saveAnswer', params);
+   }
+  }
 });
