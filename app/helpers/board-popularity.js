ab1502ca7fa671042754ae9240b1c5834de3eabf
russvetsper <russvetsper@gmail.com>
2016-11-04 11:41:55 -0700

    add glyphicon to most answered question

--- app/helpers/board-popularity.js
+++ app/helpers/board-popularity.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export function boardPopularity(params/*, hash*/) {
-  return params;
+  var board = params[0];
+
+  if(board.get('answer').get('length') >= 2) {
+   return Ember.String.htmlSafe('<span class="glyphicon  glyphicon-thumbs-up"></span>');
+ }
 }
 
 export default Ember.Helper.helper(boardPopularity);
