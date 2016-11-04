969cf530044282575192f43dbe899ccbcefb2514
russvetsper <russvetsper@gmail.com>
2016-11-04 11:05:04 -0700

    add answer popularity helper

---
+++ tests/unit/helpers/answer-popularity-test.js
@@ -0,0 +1,12 @@
+
+import { answerPopularity } from 'qa-board/helpers/answer-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | answer popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = answerPopularity([42]);
+  assert.ok(result);
+});
+
