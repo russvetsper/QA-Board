83a3d9efee980b21b3620865ecf691d91a13c68b
russvetsper <russvetsper@gmail.com>
2016-11-04 11:29:39 -0700

    update helper gile name

---
+++ app/helpers/board-popularity.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function boardPopularity(params/*, hash*/) {
+  return params;
+}
+
+export default Ember.Helper.helper(boardPopularity);
---
+++ tests/unit/helpers/board-popularity-test.js
@@ -0,0 +1,12 @@
+
+import { boardPopularity } from 'qa-board/helpers/board-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | board popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = boardPopularity([42]);
+  assert.ok(result);
+});
+
